40cb5bf68653ad500e5eb61c8e3c0af8be338793
romariolinares1 <relinare@ucsc.edu>
2024-05-07 00:44:35 -0700

    uncommented

--- src/Sprites/Player.js
+++ src/Sprites/Player.js
@@ -2,7 +2,7 @@ class Player extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, playerSpeed, leftKey, rightKey) {
         super(scene, x, y, texture, frame);
         this.left = leftKey;
-        //this.right = rightKey;
+        this.right = rightKey;
         this.playerSpeed = playerSpeed;
         scene.add.existing(this);
         return this;
